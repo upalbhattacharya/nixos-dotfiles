84196e4953e5fb50766502033f1413169c50033a
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-08-04 22:33:38 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+
+  description = "First flake";
+
+  inputs = {
+    nixpkgs = {
+      url = "nixpkgs/nixos-unstable";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+};
+
+  outputs = {self, nixpkgs, home-manager, ...}:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+    nixosConfigurations = {
+      nixos-workboots = lib.nixosSystem {
+	inherit system;
+        modules = [
+          ./configuration.nix
+	];
+      };
+    };
+    homeConfigurations = {
+      workboots = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+
+   };
+}
