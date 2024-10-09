8eed46a53f92c446c03eec9379d471c8934b0f25
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-09 14:49:54 +0200

    Update init.el

--- init.el
+++ init.el
@@ -28,10 +28,7 @@
    '("6e13ff2c27cf87f095db987bf30beca8697814b90cd837ef4edca18bdd381901" default))
  '(gac-automatically-push-p t)
  '(package-selected-packages
-   '(lsp-ui rainbow-delimiters flycheck origami org-journal helm-bibtex citar vertico git-gutter
-	    magit git-auto-commit-mode company lsp-mode org-roam-ui spacious-padding
-	    org-super-agenda fzf dashboard org-transclusion org-superstar org-modern org-roam evil
-	    catppuccin-theme)))
+   '(org-view-mode lsp-ui rainbow-delimiters flycheck origami org-journal helm-bibtex citar vertico git-gutter magit git-auto-commit-mode company lsp-mode org-roam-ui spacious-padding org-super-agenda fzf dashboard org-transclusion org-superstar org-modern org-roam evil catppuccin-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -257,3 +254,6 @@
 
 ;; citar-org-roam
 (global-set-key (kbd "C-x M-r") 'citar-open-notes) 
+
+;; org-view-mode
+(global-set-key (kbd "C-x M-e") 'org-view-mode)
