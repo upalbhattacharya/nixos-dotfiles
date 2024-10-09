6ddcf8ee695ad0540f9bc9b242039927aafd0aa4
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-09 11:15:52 +0200

    init.el

--- init.el
+++ init.el
@@ -24,7 +24,7 @@
    '("6e13ff2c27cf87f095db987bf30beca8697814b90cd837ef4edca18bdd381901" default))
  '(gac-automatically-push-p t)
  '(package-selected-packages
-   '(citar vertico git-gutter magit git-auto-commit-mode company lsp-mode org-roam-ui spacious-padding org-super-agenda fzf dashboard org-transclusion org-superstar org-modern org-roam evil catppuccin-theme)))
+   '(citar-org-roam citar vertico git-gutter magit git-auto-commit-mode company lsp-mode org-roam-ui spacious-padding org-super-agenda fzf dashboard org-transclusion org-superstar org-modern org-roam evil catppuccin-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -186,6 +186,8 @@
 ;;; citar
 (setq citar-bibliography '("~/org/bibliography.bib"))
 (setq citar-notes-paths '("~/org/academic"))
+
+;;
 
 ;;; Keybindings
 
