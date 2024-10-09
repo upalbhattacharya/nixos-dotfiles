c05974b733d85d656d5bcf25eac2a84c96c0212e
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-09 11:40:27 +0200

    init.el

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-document-title ((t (:foreground "#89b4fa" :weight bold)))))
 
 ;;; Theme
 (load-theme 'catppuccin :no-confirm)
