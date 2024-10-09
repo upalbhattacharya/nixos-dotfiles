0ba9c307978f22a85896d4b7638d2dbb3c57be68
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-09 22:07:44 +0200

    init.el

--- init.el
+++ init.el
@@ -34,7 +34,7 @@
   (default ((nil (:font "Iosevka Nerd Font" :height 220))))
   :config
   (setq inhibit-startup-screen t
-	display-line-numbers 'relative)
+	display-line-numbers '((relative)))
     (setq org-hide-emphasis-markers t)
     (menu-bar-mode -1)
     (scroll-bar-mode -1)
