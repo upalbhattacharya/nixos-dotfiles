db3ffe31ac0a52d77d5ad1a18188072f9549b2ed
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-09 14:21:07 +0200

    Update init.el

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:weight bold)))))
+ '(org-document-title ((t (:foreground "dim gray" :weight bold :height 1.0)))))
 
 ;;; Theme
 (load-theme 'catppuccin :no-confirm)
