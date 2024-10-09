6abee3a0b6845537b7543ba8e7161a3dae6c1768
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-09 11:35:24 +0200

    init.el

--- init.el
+++ init.el
@@ -136,7 +136,7 @@
          :target
          (file+head
           "${citar-citekey}.org"
-          "#+title: ${citar-citekey}\n#+created: %U\n#+last_modified: %U\n\n* ${citar-title}")
+          "#+title: ${citar-citekey}\n#+filetags: :article:\n* ${citar-title}")
          :unnarrowed t))     )
 (setq org-roam-dailies-capture-templates
       '(("d" "default" entry
