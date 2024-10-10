f1d514d4c6e60db36f0f4df4f063b22ce79458a3
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-10 22:57:16 +0200

    init.el

--- init.el
+++ init.el
@@ -278,7 +278,7 @@
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   (setq lsp-keymap-prefix "C-c l")
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (XXX-mode . lsp)
+         (python-mode . lsp)
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
