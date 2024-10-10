239c1f409169980f4e32a26e6c74bb325b824a0b
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-10 13:56:26 +0200

    init.el

--- init.el
+++ init.el
@@ -265,17 +265,8 @@
   (setq TeX-parse-self t)
   )
 
-;;; lsp-mode
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook (
-         (python-mode . lsp)
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
-
-;;; lsp-ui
-(use-package lsp-ui)
+;;; eglot
+(use-package eglot)
 
 ;;; Keybindings
 
