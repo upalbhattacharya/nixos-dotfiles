b97dc491eaa357991d06533700f116f5ad583dc8
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-10 13:59:30 +0200

    init.el

--- init.el
+++ init.el
@@ -271,9 +271,6 @@
   :config
   (add-to-list 'eglot-server-programs '(python-mode . ("pylsp")))
 
-  (setq-default eglot-workspace-configuration
-                '((:pylsp . (:configurationSources ["flake8"] :plugins (:pycodestyle (:enabled nil) :mccabe (:enabled nil) :flake8 (:enabled t))))))
-
   :hook
   ((python-mode . eglot-ensure)))
 
