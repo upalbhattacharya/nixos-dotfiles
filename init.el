7da6854b7923d0feb37c514c03e5053e5c2f8426
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-11 10:55:20 +0200

    init.el

--- init.el
+++ init.el
@@ -284,8 +284,12 @@
 ;;; reformatter
 (require 'reformatter)
 
-;; isort
-(defvar python-isort--base-args '("--quiet" "--lbt 1")
+(defcustom python-isort-command "isort"
+  "Name of the `isort` executable."
+  :group 'nasy
+  :type 'string)
+
+(defvar python-isort--base-args '("--quiet" "--atomic" "--lbt 1" "--fass")
   "Base arguments to pass to isort.")
 
 (defcustom python-isort-extra-args nil
@@ -293,18 +297,20 @@
   :group 'nasy
   :type '(repeat string))
 
-
+;;;###autoload (autoload 'python-isort-buffer "python-isort" nil t)
+;;;###autoload (autoload 'python-isort-region "python-isort" nil t)
+;;;###autoload (autoload 'python-isort-on-save-mode "python-isort" nil t)
 (reformatter-define python-isort
-  :program "isort"
-  :args (python-isort--make-args beg end) ;
+  :program python-isort-command
+  :args (python-isort--make-args beg end)
   :lighter " isort"
   :group 'python-isort)
 
 (defun python-isort--make-args (beg end)
   "Helper to build the argument list for isort for span BEG to END."
   (append python-isort--base-args
-		  python-isort-extra-args
-		  '("-")))
+          python-isort-extra-args
+          '("-")))
 
 ;;; Python-specific
 ;;; lsp-pyright
