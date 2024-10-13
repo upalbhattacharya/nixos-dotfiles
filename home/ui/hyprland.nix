a051dcfbc656af4bea09a3affbbf2dd0c9f7eb33
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-13 10:47:53 +0200

    Update files

--- home/ui/hyprland.nix
+++ home/ui/hyprland.nix
@@ -21,7 +21,7 @@ in {
 
           bind = ${modifier},Return,exec,${pkgs.kitty}/bin/kitty
           bind = ${modifier},r,exec,${pkgs.fuzzel}/bin/fuzzel
-          bind = ${modifier},q,exec,${pkgs.brave}/bin/brave
+          bind = ${modifier},q,exec,${pkgs.google-chrome}/bin/google-chrome
           bind = ${modifier},e,exec,${pkgs.emacs}/bin/emacs
           bindr = Control,SPACE,exec,${pkgs.mako}/bin/makoctl dismiss -a
           bindr = Control&Alt,l,exec,${pkgs.hyprlock}/bin/hyprlock
