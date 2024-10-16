bf6990b6404a2c139660419dd23fcf5f353ba138
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-16 14:50:02 +0200

    home/programs/papis.nix

--- home/programs/papis.nix
+++ home/programs/papis.nix
@@ -4,9 +4,7 @@
 	programs.papis = {
 		enable = true;
     libraries = {
-      papers = {
-       dir = "~/ReferenceManagement/papers";
-      };
+      papers = "~/ReferenceManagement/papers";
     };
 	};
 }
