f064deda1cbbfdbf1ebaf9de040aaf942d21a5a5
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-16 15:36:15 +0200

    init.el

--- init.el
+++ init.el
@@ -114,48 +114,92 @@
   )
 
 ;;; org-agenda using org-super-agenda
-;; Outside org because of org-super-agenda usage (?)
-(setq org-agenda-custom-commands '(
-				   ("d" "Daily" ((agenda "" (
-							     (org-agenda-span 1)
-							     (org-super-agenda-groups
-							      '(
-								(:name "Today"
-								       :auto-outline-path t
-								       :todo ("NEXT" "IN PROGRESS" "TODO")
-								       :deadline today)
-								)
-							      )
-							     )
-							 ))
-				    )
 
-				   ("o" "Overdue" ((todo "" (
-							     (org-agenda-span 1)
-							     (org-super-agenda-groups
-							      '(
-								(:name "Today"
-								       :auto-outline-path t
-								       :todo ("NEXT" "IN PROGRESS" "TODO")
-								       :deadline past)
-								)
-							      )
-							     )
-							 ))
-				    )
-				   ("w" "Week" ((agenda "" (
-							    (org-agenda-span 'week)
-							    (org-super-agenda-groups
-							     '(
-							       (:name "This Week"
-								      :auto-outline-path t
-								      :todo ("NEXT" "IN PROGRESS" "TODO"))
-							       )
-							     )
-							    )
-							))
-				    )
-				   ))
+(let ((org-agenda-span 'day)
+	  (org-super-agenda-groups
+       '(;; Each group has an implicit boolean OR operator between its selectors.
+         (:name "Today"  ; Optionally specify section name
+                :time-grid t  ; Items that appear on the time grid
+                :todo "TODAY")  ; Items that have this TODO keyword
+         (:name "Important"
+                ;; Single arguments given alone
+                :tag "bills"
+                :priority "A")
+         ;; Set order of multiple groups at once
+         (:order-multi (2 (:name "Shopping in town"
+                                 ;; Boolean AND group matches items that match all subgroups
+                                 :and (:tag "shopping" :tag "@town"))
+                          (:name "Food-related"
+                                 ;; Multiple args given in list with implicit OR
+                                 :tag ("food" "dinner"))
+                          (:name "Personal"
+                                 :habit t
+                                 :tag "personal")
+                          (:name "Space-related (non-moon-or-planet-related)"
+                                 ;; Regexps match case-insensitively on the entire entry
+                                 :and (:regexp ("space" "NASA")
+                                               ;; Boolean NOT also has implicit OR between selectors
+                                               :not (:regexp "moon" :tag "planet")))))
+         ;; Groups supply their own section names when none are given
+         (:todo "WAITING" :order 8)  ; Set order of this section
+         (:todo ("SOMEDAY" "TO-READ" "CHECK" "TO-WATCH" "WATCHING")
+                ;; Show this group at the end of the agenda (since it has the
+                ;; highest number). If you specified this group last, items
+                ;; with these todo keywords that e.g. have priority A would be
+                ;; displayed in that group instead, because items are grouped
+                ;; out in the order the groups are listed.
+                :order 9)
+         (:priority<= "B"
+                      ;; Show this section after "Today" and "Important", because
+                      ;; their order is unspecified, defaulting to 0. Sections
+                      ;; are displayed lowest-number-first.
+                      :order 1)
+         ;; After the last group, the agenda will display items that didn't
+         ;; match any of these groups, with the default order position of 99
+         )))
+  (org-agenda nil "a"))
+;; Outside org because of org-super-agenda usage (?)
+;; (setq org-agenda-custom-commands '(
+;; 				   ("d" "Daily" ((agenda "" (
+;; 							     (org-agenda-span 1)
+							    ;;  (let ((org-super-agenda-groups
+							    ;;   '(
+								;; (:name "Today"
+								;;        :auto-outline-path t
+								;;        :todo ("NEXT" "IN PROGRESS" "TODO")
+								;;        :deadline today)
+								;; )
+							    ;;   ))
+								;;    (org-agenda nil "a"))
+					;; 		 ))
+				    ;; )
+
+				   ;; ("o" "Overdue" ((todo "" (
+				   ;; 			     (org-agenda-span 1)
+				   ;; 			     (org-super-agenda-groups
+				   ;; 			      '(
+				   ;; 				(:name "Today"
+				   ;; 				       :auto-outline-path t
+				   ;; 				       :todo ("NEXT" "IN PROGRESS" "TODO")
+				   ;; 				       :deadline past)
+				   ;; 				)
+				   ;; 			      )
+				   ;; 			     )
+				   ;; 			 ))
+				   ;;  )
+				   ;; ("w" "Week" ((agenda "" (
+				   ;; 			    (org-agenda-span 'week)
+				   ;; 			    (org-super-agenda-groups
+				   ;; 			     '(
+				   ;; 			       (:name "This Week"
+				   ;; 				      :auto-outline-path t
+				   ;; 				      :todo ("NEXT" "IN PROGRESS" "TODO"))
+				   ;; 			       )
+				   ;; 			     )
+				   ;; 			    )
+				   ;; 			))
+				   ;;  )
+				   ;; ))
 
 (use-package org-super-agenda
   :config
