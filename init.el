36aecdeeffe026846f5c6d7997cf8529c36ae4fe
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-16 08:48:54 +0200

    init.el

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
    '(:foreground default :background default :scale 2.2 :html-foreground "Black" :html-background "Transparent" :html-scale 2.0 :matchers
 				 ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(package-selected-packages
-   '(org-roam-bibtex annotate toc-org direnv hotfuzz ruff-format nix-mode git-auto-commit lsp-ui lsp-mode latex-extra latexdiff auctex org-view-mode rainbow-delimiters flycheck origami org-journal vertico git-gutter magit git-auto-commit-mode company org-roam-ui spacious-padding org-super-agenda fzf dashboard org-transclusion org-superstar org-modern org-roam evil catppuccin-theme))
+   '(helm-bibtex org-roam-bibtex annotate toc-org direnv hotfuzz ruff-format nix-mode git-auto-commit lsp-ui lsp-mode latex-extra latexdiff auctex org-view-mode rainbow-delimiters flycheck origami org-journal vertico git-gutter magit git-auto-commit-mode company org-roam-ui spacious-padding org-super-agenda fzf dashboard org-transclusion org-superstar org-modern org-roam evil catppuccin-theme))
  '(python-isort-extra-args nil))
 
 ;;; Theme
@@ -241,6 +241,9 @@
 	(rst-mode . bibtex-completion-format-citation-sphinxcontrib-bibtex) 
 	(default . bibtex-completion-format-citation-default)))
   )
+
+;;; helm-bibtex
+(use-package helm-bibtex)
 
 ;;; company
 (use-package company
