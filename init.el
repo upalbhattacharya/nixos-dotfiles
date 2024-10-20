47189e8ba1f1e1cc5250a6735f5c8bd19f776ec1
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-20 19:05:18 +0200

    init.el

--- init.el
+++ init.el
@@ -169,7 +169,7 @@
   (setq org-roam-capture-templates
 	'(("d" "default" plain "%?"
 	   :target (file+head "nodes/${title}.org"
-						  ":PROPERTIES:\n:CATEGORY: NODE\n:END:\n#+TITLE: ${title}\n#+FILETAGS:\n\n* ${title}\n\n** Table of Contents :TOC_4:\n")
+						  ":PROPERTIES:\n:CATEGORY: NODE\n:END:\n#+TITLE: ${title}\n#+FILETAGS:\n\n* ${title}\n\n")
 	   :immediate-finish t
 	   :create-file yes
 	   :unnarrowed t)
