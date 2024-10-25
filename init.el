dce471275b0351ea1f16284d53222c83972f088f
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-25 10:33:33 +0200

    init.el

--- init.el
+++ init.el
@@ -108,7 +108,7 @@
 				(agenda . " %?-12c  %?-12t%?-b ")
 				(todo . " %?-12t %s")))
   (setq org-agenda-view-columns-initially t)
-  (setq org-agenda-overriding-columns-format "%CATEGORY %TODO(STATUS) %PRIORITY%DEADLINE %SCHEDULED %ITEM")
+  (setq org-agenda-overriding-columns-format "%CATEGORY %TODO(STATUS) %PRIORITY %DEADLINE %SCHEDULED %ITEM")
 
   ;; org-todo
   (setq org-todo-keywords
