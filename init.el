6fecd1c0440d41101333b4fa686619f3dc8948f1
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-25 11:53:21 +0200

    init.el

--- init.el
+++ init.el
@@ -535,7 +535,7 @@ exist after each headings's drawers."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-agenda-structure-filter ((t nil))))
 
 ;; unpackaged/org-fix-blank-lines
 (global-set-key (kbd "C-c f") 'unpackaged/org-fix-blank-lines)
