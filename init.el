e0f8f7abf4387f9727f702511dba629a0895c521
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-27 19:16:13 +0100

    init.el

--- init.el
+++ init.el
@@ -197,7 +197,7 @@
 			      "#+TITLE: ${citekey}\n#+FILETAGS: :article:\n* ${title}")
            :unnarrowed t)
 	("c" "capture" plain "%?"
-			:target (file+head+olp "worklogs/log_%<%Y-%m>.org"
+			:target (file+olp "worklogs/log_%<%Y-%m>.org"
 									":PROPERTIES:\n:CATEGORY: CAPTURE\n:ID:\n:END:#+TITLE: ${title}\n\n"
 									("%<%Y-W%W>" "%<%Y-%m-%d>" "TODO ${title}\n:PROPERTIES:\n:CATEGORY: CAPTURE\n:ID:\n:END:#+TITLE: ${title}\n\n")))
 	  ))
