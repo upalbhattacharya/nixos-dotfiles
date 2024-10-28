7e07c1a940d45cfc1f28e9bdbb18d47eb74c730e
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-28 15:24:17 +0100

    init.el

--- init.el
+++ init.el
@@ -114,6 +114,11 @@
 				(agenda . " %?-12c  %?-12t%?-b ")
 				(todo . " %?-12t %s")))
   (setq org-agenda-view-columns-initially t)
+	(defun file-base-name (column-title value)
+	"Modifies the value to display in column view."
+	(when (equal column-title "FILE")
+	  (file-name-base value)))
+	(add-to-list 'org-columns-summary-types '("filenamebase" . file-base-name))
   (setq org-agenda-overriding-columns-format "%15CATEGORY %15TODO(STATUS) %PRIORITY(PR.) %DEADLINE %SCHEDULED %120ITEM")
   (setq org-agenda-with-colors t)
   (setq org-agenda-format-date (lambda (date) (concat "\n"
