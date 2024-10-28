1f2d2e722f99511e7c24c8ecd051e8a91f832e68
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-28 14:31:14 +0100

    init.el

--- init.el
+++ init.el
@@ -184,6 +184,7 @@
           (alltodo "" ((org-agenda-overriding-header "")
                        (org-super-agenda-groups
                         '((:name "Forgotten"
+								 :date nil
                                  :deadline nil
 								 :scheduled nil
                                 :order 1
