9f648671dc1fc3b218966d4c1d9289d3f62821ae
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-10-28 14:08:45 +0100

    init.el

--- init.el
+++ init.el
@@ -146,9 +146,73 @@
 (use-package org-super-agenda
   :after org
   :init
+  (setq org-agenda-custom-commands (list(quote
+        ("z" "Super zaen view" (
+                                (agenda "" ((org-agenda-span 'day)
+                      (org-super-agenda-groups
+                       '((:name "Today"
+                                :time-grid t
+                                :date today
+                                :todo "TODAY"
+                                :scheduled today
+                                :order 1
+                                )))))
+          (alltodo "" ((org-agenda-overriding-header "")
+                       (org-super-agenda-groups
+                        '((:name "Next to do"
+                                 :todo "NEXT"
+                                 :order 1)
+                          (:name "Important"
+                                 :tag "Important"
+                                 :priority "A"
+                                 :order 6)
+                          (:name "Due Today"
+                                 :deadline today
+                                 :order 2)
+                          (:name "Due Soon"
+                                 :deadline future
+                                 :order 8)
+                          (:name "Overdue"
+                                 :deadline past
+                                 :order 7)
+                          (:name "Assignments"
+                                 :tag "Assignment"
+                                 :order 10)
+                          (:name "Issues"
+                                 :tag "Issue"
+                                 :order 12)
+                          (:name "Projects"
+                                 :tag "Project"
+                                 :order 14)
+                          (:name "Emacs"
+                                 :tag "Emacs"
+                                 :order 13)
+                          (:name "Research"
+                                 :tag "Research"
+                                 :order 15)
+                          (:name "To read"
+                                 :tag "Read"
+                                 :order 30)
+                          (:name "Waiting"
+                                 :todo "WAITING"
+                                 :order 20)
+                          (:name "trivial"
+                                 :priority<= "C"
+                                 :tag ("Trivial" "Unimportant")
+                                 :todo ("SOMEDAY" )
+                                 :order 90)
+                          (:discard (:tag ("Chore" "Routine" "Daily")))
+                          ))
+                       )))
+         ) ; Super zaen view
+        )))
+
+
+
+
 	(setq org-agenda-custom-commands
 		'(
-					("u" "Super View" ((agenda "" (
+					("t" "Today" ((agenda "" (
 									(org-agenda-span 'day)
 									(org-super-agenda-groups
 									 '(
