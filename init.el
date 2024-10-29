09428ce63181b7dd32b5f847475b180004f72321
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-29 10:34:36 +0100

    init.el

--- init.el
+++ init.el
@@ -156,7 +156,7 @@
                                 (agenda "" ((org-agenda-span 'day)
                       (org-super-agenda-groups
                        '((:name "Today"
-                                :todo ("TODAY")
+                                :todo "TODAY"
                                 :order 1
                                 )
 						 (:discard (:anything t))
