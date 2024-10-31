6ff5cfa351dadfe48f75fb5256913d5172dc3610
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-10-31 21:36:29 +0100

    init.el

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
    '("~/org/journal/202410.org" "~/org/nodes/CFLW Data.org" "~/org/nodes/Dutch Vocabulary.org" "~/org/nodes/EKAW 2024.org" "~/org/nodes/ESWC 2025.org" "~/org/nodes/Emacs.org" "~/org/nodes/NLDR Ranking.org" "~/org/nodes/PhD Meetings.org" "~/org/nodes/Recurring.org" "~/org/nodes/Routledge Dutch Intensive Course.org" "~/org/nodes/Systematic Literature Review.org" "~/org/nodes/Term Typing Ontology Enrichment Experiment 1.org" "~/org/nodes/UU ICS Server.org" "~/org/nodes/Who Am I.org" "~/org/worklogs/Log 2024-10.org" "~/org/worklogs/log_2024-10-08.org" "~/org/worklogs/log_2024-10-09.org" "~/org/worklogs/log_2024-10-10.org" "~/org/worklogs/log_2024-10-11.org" "~/org/log_2024-10.org"))
  '(org-agenda-sorting-strategy
    '((agenda habit-down time-up priority-down category-keep)
-     (todo deadline-up scheduled-up time-up priority-down category-keep)
+     (todo deadline-up scheduled-up time-up category-keep)
      (tags priority-down category-keep)
      (search category-keep)))
  '(org-format-latex-options
@@ -167,6 +167,7 @@
            ((agenda
              ""
              ((org-agenda-span 'day)
+              (org-agenda-sorting-strategy '(deadline-up scheduled-up priority-down))
               (org-super-agenda-groups
                '((:name "Due" :and (:not (:log closed) :deadline today) :order 1)
                  (:name "Scheduled" :and (:not (:log closed) :scheduled today) :order 2)
