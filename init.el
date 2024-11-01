6640dd9239f794fd33101157dac7e0a6e26ee1c5
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-11-01 11:38:59 +0100

    init.el

--- init.el
+++ init.el
@@ -240,7 +240,11 @@
             ((org-agenda-overriding-header "Past and Future")
              (org-super-agenda-groups
               '((:name "Overdue" :deadline past :order 1)
-                (:name "Due Soon" :and (:not (:todo "TODAY") :deadline future :not(:property ("CATEGORY" "PERSONAL"))) :order 2)
+                (:name
+                 "Due Soon"
+                 :and
+                 (:not (:todo "TODAY") :deadline future :not (:property ("CATEGORY" "PERSONAL")))
+                 :order 2)
                 (:name "Upcoming" :and (:not (:todo "TODAY") :scheduled future) :order 3)
                 (:name "Later" :todo "LATER" :order 4)
                 (:name
