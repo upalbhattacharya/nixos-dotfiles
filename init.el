e822351c59cf316c861657a7ff57c94581af9620
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-11-03 21:06:41 +0100

    init.el

--- init.el
+++ init.el
@@ -161,7 +161,7 @@
         '(("j"
            "journal"
            plain
-           (file+olp+datetree "journal/Journal %<%Y-%m>.org" "%<%Y-%m-%d>"))))
+           (file+olp+datetree "journal/Journal %<%Y-%m>.org"))))
 
   ;;org-cite
   (setq org-cite-global-bibliography '("~/org/bibliography.bib"))
