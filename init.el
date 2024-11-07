c0978a5350910e375877a100c67aa5904839c155
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-07 15:46:32 +0100

    init.el

--- init.el
+++ init.el
@@ -308,7 +308,12 @@ if nil,the top of the file."
  (setq org-cite-global-bibliography '("~/org/bibliography.bib"))
 
  ;; org-agenda
- (setq org-agenda-files (list org-agenda-de
+ (setq org-agenda-files (list org-default-inbox-file
+                              org-default-projects-dir
+                              org-default-areas-dir
+                              org-default-resources-dir
+                              org-default-archive-dir)
+ (setq org-agenda-file-regexp "^[a-z0-9-_]+.org")
  (setq org-agenda-start-day "+0d")
  (setq org-agenda-window-setup 'other-tab)
  (setq org-agenda-skip-timestamp-if-done t)
