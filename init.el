0dbc9edbaa4c7968a8c43fe8f10cac0d491f63e8
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-07 22:08:20 +0100

    init.el

--- init.el
+++ init.el
@@ -418,7 +418,7 @@ otherwise use the subtree title."
  (setq org-roam-dailies-directory "~/org/Journal/")
  (setq org-roam-completion-everywhere t)
  (setq org-roam-capture-templates
-       '(("d" "default" entry "* %?"
+       '(("d" "default" entry "* ${title}%?"
           :target
           (file+head
            "Inbox.org" ":PROPERTIES:\n:SPACE: WORK\n:END:")
