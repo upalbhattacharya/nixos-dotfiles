4cee599d31641f293c9eeca3b224208199454a00
Upal Bhattacharya <u.bhattacharya@uu.nl>
2024-11-07 16:46:36 +0100

    init.el

--- init.el
+++ init.el
@@ -222,6 +222,30 @@ if nil,the top of the file."
   (interactive)
   (org-refile-subtree-to-file org-default-projects-dir))
 
+(defun me/org-file-from-subtree (&optional name)
+"Cut the subtree currently being edited and create a new file
+from it.
+
+If called with the universal argument, prompt for new filename,
+otherwise use the subtree title."
+(interactive "P")
+(org-back-to-heading)
+(let ((filename (cond
+               (current-prefix-arg
+                (expand-file-name
+                 (read-file-name "New file name: ")))
+               (t
+                (concat
+                 (expand-file-name
+                  (org-element-property :title
+                                        (org-element-at-point))
+                  default-directory)
+                 ".org")))))
+(org-cut-subtree)
+(find-file-noselect filename)
+(with-temp-file filename
+  (org-mode)
+  (yank))))
 ;;; emacs
 (use-package
  emacs
