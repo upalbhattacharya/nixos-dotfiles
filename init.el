5617798809d29f03aa3c13435e1febcffe51d853
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-11-08 11:56:21 +0100

    init.el

--- init.el
+++ init.el
@@ -293,9 +293,8 @@
              ""
              ((org-agenda-span 'day)
               (org-ql-search '(org-agenda-files)
-                '(and (or 
-                       (deadline :to today)
-                       (scheduled :to today))
+                '(and (or (deadline :to today)
+                          (scheduled :to today))
                       (not (done)))
                 :title "My Agenda View"
                 ;; The `org-super-agenda-groups' setting is used automatically when set, or it
