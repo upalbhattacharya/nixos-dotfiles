689a9a5c44f3da7cc761f0de42d693319a5321c0
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-11-08 13:43:45 +0100

    init.el

--- init.el
+++ init.el
@@ -411,7 +411,7 @@ exist after each headings's drawers."
 (use-package org-ql
   :init
   (setq org-agenda-custom-commands
-        '(("z" "Zen View"
+        '(("z" "Zen View Today"
            (
             (org-ql-block '(and (todo)
                                 (deadline :on today)
