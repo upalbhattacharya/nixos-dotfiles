0199e761eeb7954440d00096786bacf1e31840b5
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-11-14 09:55:23 +0100

    init.el

--- init.el
+++ init.el
@@ -264,6 +264,7 @@
   :custom
   (citar-bibliography '("~/org/bibliography.bib"))
   (citar-notes-path '("~/org/Literature/"))
+  (citar-file-notes-extensions '("org"))
   :hook
   (LaTeX-mode . citar-capf-setup)
   (org-mode . citar-capf-setup))
