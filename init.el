89fe22119e22d163cdf44f6f421b70b85b4683a2
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-11-14 12:42:55 +0100

    init.el

--- init.el
+++ init.el
@@ -262,12 +262,6 @@
  (setq citar-org-roam-note-title-template "${title}")
  (setq citar-org-roam-capture-template-key "l"))
 
-;; org-roam-bibtex
-;; (use-package org-roam-bibtex
-;;   :after citar-org-roam
-;;   :config
-;;   (setq org-roam-bibtex-mode 1))
-
 ;;; company
 (use-package company :config (company-mode 1))
 
