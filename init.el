71710638725014dde8fa8b7139f05221ca9b4f82
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-11-17 15:50:28 +0100

    init.el

--- init.el
+++ init.el
@@ -628,6 +628,18 @@ exist after each headings's drawers."
  ;; Make it active
 (ad-activate 'org-insert-todo-heading)
 
+ (defadvice org-insert-heading (after workboots/insert-todo-metadata activate)
+   "Insert a CREATED property using org-expiry.el for TODO entries"
+   (workboots/insert-todo-metadata))
+ ;; Make it active
+(ad-activate 'org-insert-heading)
+
+ (defadvice org-insert-subheading (after workboots/insert-todo-metadata activate)
+   "Insert a CREATED property using org-expiry.el for TODO entries"
+   (workboots/insert-todo-metadata))
+ ;; Make it active
+(ad-activate 'org-insert-subheading)
+
 ;;; Keybindings
 
 ;; General
