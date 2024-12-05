d8709627dcb7cc7f09d1475b8e901affbf62f6f0
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-12-05 08:58:25 +0100

    Update files

--- home/ui/kanshi.nix
+++ home/ui/kanshi.nix
@@ -1,36 +1,56 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 {
-	services.kanshi = {
-		enable = true;
+  services.kanshi = {
+    enable = true;
     systemdTarget = "";
-		settings = [
-			{
-				profile.name = "solo";
-				profile.outputs = [
-					{
-						criteria = "eDP-1";
-						status = "enable";
-						mode = "2560x1440@165.003Hz";
-						position = "0,0";
-					}
-				];
+    settings = [
+      {
+        profile.name = "solo";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            mode = "2560x1440@165.003Hz";
+            position = "0,0";
+          }
+        ];
       }
       {
-				profile.name = "home";
-				profile.outputs = [
-					{
-						criteria = "eDP-1";
-						status = "disable";
-					}
+        profile.name = "home-utrecht";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
           {
             criteria = "LG Electronics LG ULTRAGEAR+ 406NTYT11186";
             status = "enable";
             mode = "3840x2160@144.05Hz";
             position = "0,0";
           }
-				];
-			}
-		];
-	};
+        ];
+      }
+      {
+        profile.name = "home-freiburg";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+          {
+            criteria = "LG Electronics LG ULTRAGEAR+ 405NTPC4L824";
+            status = "enable";
+            mode = "3840x2160@144.05Hz";
+            position = "0,0";
+          }
+        ];
+      }
+    ];
+  };
 }
