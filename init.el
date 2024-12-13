b16ee35d0b3586502b1abb4fed8338992ef68063
upalbhattacharya <upal.bhattacharya@gmail.com>
2024-12-13 09:26:37 +0100

    init.el

--- init.el
+++ init.el
@@ -1,4 +1,44 @@
-(require 'package)
+;;; Bootstrap elpaca
+(defvar elpaca-installer-version 0.8)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (< emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (load "./elpaca-autoloads")))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+
+;; (require 'package)
 
 ;; When the official repos are down
 ;; (setq package-archives
@@ -6,18 +46,18 @@
 ;;         ("org"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/org/")
 ;;         ("gnu"   . "https://raw.githubusercontent.com/d12frosted/elpa-mirror/master/gnu/")))
  
-(setq package-archives
-      '(("melpa" . "https://melpa.org/packages/")))
- ;;; (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
- (package-initialize)
+;; (setq package-archives
+;;       '(("melpa" . "https://melpa.org/packages/")))
+;;  ;;; (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+;;  (package-initialize)
 
- (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
- (eval-and-compile
-   (setq
-   use-package-always-ensure t
-   use-package-expand-minimally t))
+;;  (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+;;  (eval-and-compile
+;;    (setq
+;;    use-package-always-ensure t
+;;    use-package-expand-minimally t))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -83,12 +123,16 @@
    '(gnu-elpa gnu-elpa-keyring-update avy hydra citar-org-roam citar elisp-autofmt aggressive-indent evil-nerd-commenter envrc which-key org-anki org-ql annotate toc-org hotfuzz ruff-format nix-mode git-auto-commit lsp-ui lsp-mode latex-extra latexdiff auctex org-view-mode rainbow-delimiters flycheck origami vertico git-gutter magit git-auto-commit-mode company org-roam-ui spacious-padding fzf dashboard org-transclusion org-superstar org-roam evil catppuccin-theme))
  '(python-isort-extra-args nil))
 
+;;; Use elpaca use-package
+(elpaca elpaca-use-package
+  (elpaca-use-package-mode))
+(setq use-package-always-ensure t)
+
 ;;; Theme
 (load-theme 'catppuccin :no-confirm)
 
 ;;; emacs
-(use-package
- emacs
+(use-package emacs
  :custom-face (default ((nil (:font "Iosevka Nerd Font" :height 220))))
  :hook (org-mode . auto-fill-mode)
  :config
