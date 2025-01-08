30dfd865a352580e0b5aa1577dcd0fc217b6ff5f
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-08 10:35:46 +0100

    home/ui/catppuccin.nix

---
+++ home/ui/catppuccin.nix
@@ -0,0 +1,11 @@
+{pkgs, lib, contrib }:
+{
+        catppuccin = {
+        mako.enable = true;
+        hyprland.enable = true;
+        zsh-syntax-highlighting.enable = true;
+        starship.enable = true;
+        zellij.enable = true;
+        swaylock.enable = true;
+       };
+}
