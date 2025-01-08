f255cf5555c350fa79aa0def3a25264e60d97633
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-08 10:38:53 +0100

    home/shell/default.nix

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -8,7 +8,7 @@
   	enable = true;
   	syntaxHighlighting = {
       enable = true;
-      catppuccin.enable = true;
+      # catppuccin.enable = true;
     };
   	autosuggestion.enable = true;
   	enableCompletion = true;
@@ -19,7 +19,7 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
-    catppuccin.enable = true;
+    # catppuccin.enable = true;
     settings = {
       directory = {
         truncation_length = 0;
