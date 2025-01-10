4ceb5ce868381c24ea873a050e7e1b23513e67cc
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-10 12:19:56 +0100

    init.el

--- init.el
+++ init.el
@@ -74,7 +74,7 @@
  '(gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
  '(org-agenda-block-separator 46)
  '(org-agenda-breadcrumbs-separator " -> ")
- '(org-agenda-files '("/home/upal/org/marginalia.org"))
+ '(org-agenda-files nil)
  '(org-export-backends '(ascii html icalendar latex odt org))
  '(org-format-latex-options
    '(:foreground default :background default :scale 2.2 :html-foreground "Black" :html-background "Transparent" :html-scale 2.0 :matchers
