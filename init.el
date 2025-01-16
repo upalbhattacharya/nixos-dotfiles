6cc0eefc5d349e2ff91b8a0fa322426883ef371b
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-01-16 14:56:53 +0100

    init.el

--- init.el
+++ init.el
@@ -347,6 +347,7 @@
           ("r" "resource" entry "* TODO ${title}\n:PROPERTIES:\n:NAME:\t${title}\n:ID:\t%(org-id-uuid)\n:CREATED:\t%U\n:END:\n
 [[org-ql-search:(and (todo) (not(done)) (level 2) (property \"NAME\" \"${title}\" inherit))][org-ql-search: Pending Tasks]]"
            :target (file "Resources.org") :empty-lines 1)
+
           ("f" "fleeting note" plain "%?"
            :target
            (file+olp
