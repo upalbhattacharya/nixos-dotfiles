a1296c6a8b06c22512ebbd748d7630180b6b7ff2
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-01-16 11:57:30 +0100

    init.el

--- init.el
+++ init.el
@@ -849,7 +849,7 @@
 (defun workboots/org-present-end ()
   ;; Stop centering the document
   (org-tidy-untidy-buffer)
-  (setq-local face-remapping-alist '((default variable-pitch default)))
+  (setq-local face-remapping-alist '((variable-pitch default)))
   (org-present-show-cursor)
   (org-present-read-write))
 
