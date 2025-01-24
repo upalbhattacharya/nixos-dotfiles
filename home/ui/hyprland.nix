be780f8538b4ff3e348a798d61ce0b6d461d4a64
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-24 13:08:20 +0100

    home/ui/hyprland.nix

--- home/ui/hyprland.nix
+++ home/ui/hyprland.nix
@@ -152,7 +152,8 @@ in
       binde=,g,exec,wlrctl pointer scroll 0 10
 
       # eww visibility
-      bindrn = SUPER_SHIFT,e, exec, eww update show=false
+      bind = SUPER, SUPER_L, exec, eww update show=true
+      bindr = SUPER, SUPER_L, exec, eww update show=false
 
       # Exit cursor submap
       # If you do not use cursor timeout or cursor:hide_on_key_press, you can delete its respective cals
