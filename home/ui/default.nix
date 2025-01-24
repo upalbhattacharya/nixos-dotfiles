81a39612957cb222a7fa0af05c520a17e359f802
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-24 11:16:10 +0100

    Update files

--- home/ui/default.nix
+++ home/ui/default.nix
@@ -1,16 +1,22 @@
-{ config, lib, pkgs, ... }:
 {
-	imports = [
-		# ./colors.nix
-		./fuzzel.nix
-		./kanshi.nix
-		# ./keybindings.nix
-		# ./sway.nix
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    # ./colors.nix
+    ./fuzzel.nix
+    ./kanshi.nix
+    # ./keybindings.nix
+    # ./sway.nix
     ./hyprland.nix
     ./hyprlock.nix
-		./swaylock.nix
-		./waybar.nix
-		./wpaperd.nix
-		./catppuccin.nix
-	];
+    ./swaylock.nix
+    ./waybar.nix
+    ./eww.nix
+    ./wpaperd.nix
+    ./catppuccin.nix
+  ];
 }
