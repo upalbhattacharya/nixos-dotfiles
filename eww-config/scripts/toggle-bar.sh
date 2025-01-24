f1b9620fa68c5c3e61b446abd3a54fd974260106
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-24 14:35:56 +0100

    eww-config/scripts/toggle-bar.sh

--- eww-config/scripts/toggle-bar.sh
+++ eww-config/scripts/toggle-bar.sh
@@ -3,12 +3,19 @@
 SHOW=$(eww get show)
 KEY=$(xev -1 | awk '{gsub(/),/,""); print $23}')
 STATE=$(xev -1 | awk '{print $1}')
-if [[ $STATE="KeyPress" && $KEY="SUPER_L" ]]
-then
-    eww update show=true
-fi
 
-if [[ $STATE="KeyRelease" && $KEY="SUPER_L" ]]
-then
-    eww update show=false
-fi
+while
+do
+
+    if [[ $STATE="KeyPress" && $KEY="SUPER_L" ]]
+    then
+        eww update show=true
+    fi
+
+    if [[ $STATE="KeyRelease" && $KEY="SUPER_L" ]]
+    then
+        eww update show=false
+    fi
+    sleep 2
+done
+
