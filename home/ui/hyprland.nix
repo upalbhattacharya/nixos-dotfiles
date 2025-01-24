f6eec0559e1fd04bae5c16f49af8091bb77d27f0
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-24 13:44:46 +0100

    home/ui/hyprland.nix

--- home/ui/hyprland.nix
+++ home/ui/hyprland.nix
@@ -122,6 +122,7 @@ in
       bindl = ,switch:off:[Lid Switch],exec,hyprctl keyword monitor "eDP-1,enable"
 
       # eww visibility
+      bindn = ${modifier},${modifier}_L,exec,${pkgs.hyprland}/bin/hyprctl dispatch exec eww update show=true
       bindrn = ${modifier},${modifier}_L,exec,${pkgs.hyprland}/bin/hyprctl dispatch exec eww update show=false
 
       bind=${modifier},Backspace,exec,hyprctl keyword cursor:inactive_timeout 0; hyprctl keyword cursor:hide_on_key_press false; hyprctl dispatch submap cursor
