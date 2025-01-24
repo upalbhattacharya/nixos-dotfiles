372cedafa6156e778a02cb8e5326e447758ad335
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-24 13:37:26 +0100

    home/ui/hyprland.nix

--- home/ui/hyprland.nix
+++ home/ui/hyprland.nix
@@ -153,8 +153,7 @@ in
 
       # eww visibility
 
-      bind = ${modifier}_SHIFT,e,exec,${pkgs.hyprland}/bin/hyprctl dispatch exec eww update show=false
-      bindr = ${modifier}_SHIFT,e, exec,hyprctl dispatch exec eww update show=false
+      bind = ${modifier}_SHIFT,y,exec,${pkgs.hyprland}/bin/hyprctl dispatch exec eww update show=false
 
       # Exit cursor submap
       # If you do not use cursor timeout or cursor:hide_on_key_press, you can delete its respective cals
