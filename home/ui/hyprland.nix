47a8ccd52c029957f1e5ee77c41b540f64822404
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-25 12:37:20 +0100

    home/ui/hyprland.nix

--- home/ui/hyprland.nix
+++ home/ui/hyprland.nix
@@ -124,8 +124,8 @@ in
       bind=${modifier},Backspace,exec,hyprctl keyword cursor:inactive_timeout 0; hyprctl keyword cursor:hide_on_key_press false; hyprctl dispatch submap cursor
       bind=${modifier},plus,exec,wl-kbptr && (hyprctl keyword cursor:inactive_timeout 0; hyprctl keyword cursor:hide_on_key_press false; hyprctl dispatch submap cursor)
       # eww toggle
-      bindo = ${modifier}, ${modifier}_L, exec, ${pkgs.eww}/bin/eww open-many --toggle central-bar left-bar right-bar
-      bindr = ${modifier}, ${modifier}_L, exec, ${pkgs.eww}/bin/eww open-many --toggle central-bar left-bar right-bar
+      bindon = ${modifier}, ${modifier}_L, exec, ${pkgs.eww}/bin/eww open-many --toggle central-bar left-bar right-bar
+      bindrn = ${modifier}, ${modifier}_L, exec, ${pkgs.eww}/bin/eww open-many --toggle central-bar left-bar right-bar
 
       # Cursor submap (similar to the Mouse mode in Sway)
       submap=cursor
