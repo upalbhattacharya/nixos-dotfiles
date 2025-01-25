de7a8f5991370467a779fcea26a68434df23daa5
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-25 16:36:38 +0100

    eww-config/scripts/show-bar.sh

--- eww-config/scripts/show-bar.sh
+++ eww-config/scripts/show-bar.sh
@@ -2,6 +2,7 @@
 
 BAR=$(hyprctl layers | grep "eww-bar" | wc -l)
 if [[ $BAR -eq 0 ]]; then
-    $(eww open-many central-bar left-bar right-bar)
+    notify-send "Creating bar!" "$BAR"
+    eww open-many central-bar left-bar right-bar
 fi
 
