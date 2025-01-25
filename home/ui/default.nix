a3b617d4376f2c9384388125efcc2a78099fbee6
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-25 11:12:15 +0100

    Update files

--- home/ui/default.nix
+++ home/ui/default.nix
@@ -9,12 +9,11 @@
     # ./colors.nix
     ./fuzzel.nix
     ./kanshi.nix
-    # ./keybindings.nix
-    # ./sway.nix
     ./hyprland.nix
     ./hyprlock.nix
     ./swaylock.nix
     ./waybar.nix
+    ./eww.nix
     ./wpaperd.nix
     ./catppuccin.nix
   ];
