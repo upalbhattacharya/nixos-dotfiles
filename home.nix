18826c14b887857f2e80631296efd73ecbc9f652
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-26 11:58:18 +0100

    Update files

--- home.nix
+++ home.nix
@@ -1,48 +1,56 @@
-{ config, pkgs, catppuccin, ... }: let
-	name = "workboots";
-	version = "24.05";
+{
+  config,
+  pkgs,
+  catppuccin,
+  ...
+}:
+let
+  name = "workboots";
+  version = "24.05";
 in
 {
-	imports = [
-		./home
-	];
+  imports = [
+    ./home
+  ];
 
-  	nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-	home = {
-		username = "${name}";
-		homeDirectory = "/home/${name}";
-		stateVersion = "${version}";
-		sessionVariables = {
- 			
-			# session.
-          		# XDG_CURRENT_DESKTOP = "hyprland";
-          		# XDG_SESSION_DESKTOP = "hyprland";
-          		# XDG_SESSION_TYPE = "wayland";
+  home = {
+    username = "${name}";
+    homeDirectory = "/home/${name}";
+    stateVersion = "${version}";
+    sessionVariables = {
 
-          		# wayland stuff.
-          		# QT_QPA_PLATFORM="wayland";
-          		# QT_WAYLAND_DISABLE_WINDOWDECORATION="1";
-          		# SDL_VIDEODRIVER = "wayland";
+      # session.
+      # XDG_CURRENT_DESKTOP = "hyprland";
+      # XDG_SESSION_DESKTOP = "hyprland";
+      # XDG_SESSION_TYPE = "wayland";
 
-          		# apps stuff.
-          		_JAVA_AWT_WM_NONREPARENTING=1;
-              WLR_RENDERER="vulkan";
-              WLR_NO_HARDWARE_CURSORS=1;
-              XWAYLAND_NO_GLAMOR=1;
+      # wayland stuff.
+      # QT_QPA_PLATFORM="wayland";
+      # QT_WAYLAND_DISABLE_WINDOWDECORATION="1";
+      # SDL_VIDEODRIVER = "wayland";
 
-		};
-		packages = [
-			pkgs.localsend
-			pkgs.zotero
+      # apps stuff.
+      _JAVA_AWT_WM_NONREPARENTING = 1;
+      WLR_RENDERER = "vulkan";
+      WLR_NO_HARDWARE_CURSORS = 1;
+      XWAYLAND_NO_GLAMOR = 1;
+
+    };
+    packages = [
+      pkgs.localsend
+      pkgs.zotero
       pkgs.anki-bin
       pkgs.wl-clipboard-rs
       pkgs.ripgrep
-		];
-		file = {};
-	};
-	catppuccin.flavor = "mocha";
-	catppuccin.enable = true;
-	programs.home-manager.enable = true;
+      pkgs.jq
+      pkgs.socat
+    ];
+    file = { };
+  };
+  catppuccin.flavor = "mocha";
+  catppuccin.enable = true;
+  programs.home-manager.enable = true;
 
 }
