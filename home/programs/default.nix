3632b5157fe0320dea0a2b104f8571a4464bf945
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-31 09:24:00 +0100

    Update files

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -1,13 +1,13 @@
 { ... }:
 {
-	imports = [
-		./brave.nix
-    ./google-chrome.nix
-		./fzf.nix
-		./git.nix
-		./kitty.nix
+  imports = [
+    ./brave.nix
+    ./firefox.nix
+    ./fzf.nix
+    ./git.nix
+    ./kitty.nix
     # ./emacs.nix
     ./compression.nix
     ./mpv.nix
-	];
+  ];
 }
