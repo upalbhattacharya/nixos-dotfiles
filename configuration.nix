a35cd8cf15fd55ec3ff5316d628c71007cd4f536
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-31 10:46:54 +0100

    configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -74,7 +74,7 @@ in
   # };
   services.greetd.settings = {
     default_session = {
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-session --cmd dwl";
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-session --cmd dwl -s kitty";
     };
   };
 
