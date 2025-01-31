ee29f81692419a7436101fdbfd32a94a74d36a04
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-01-31 10:32:33 +0100

    Update files

--- configuration.nix
+++ configuration.nix
@@ -67,16 +67,16 @@ in
   services.xserver.displayManager.gdm.autoSuspend = false;
 
   services.greetd.enable = true;
-  # services.greetd.settings = {
-  #   default_session = {
-  #     command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-session --cmd hyprland";
-  #   };
-  # };
   services.greetd.settings = {
     default_session = {
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-session --cmd 'dwl \> /tmp/dwltags'";
+      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-session --cmd hyprland";
     };
   };
+  #  services.greetd.settings = {
+  #   default_session = {
+  #     command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-session --cmd dwl";
+  #  };
+  # };
 
   # Nvidia
   hardware.graphics = {
@@ -134,7 +134,26 @@ in
   services.blueman.enable = true;
 
   # TLP
-  services.tlp.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+
+    };
+  };
 
   # Cron
   services.cron = {
--- dwl-config.h
+++ dwl-config.h
@@ -10,6 +10,7 @@ static const unsigned int borderpx         = 1;  /* border pixel of windows */
 static const float bordercolor[]           = COLOR(0x444444ff);
 static const float focuscolor[]            = COLOR(0x005577ff);
 static const float urgentcolor[]           = COLOR(0xff0000ff);
+static const float rootcolor[]           = COLOR(0xff0000ff);
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0}; /* You can also use glsl colors */
 
