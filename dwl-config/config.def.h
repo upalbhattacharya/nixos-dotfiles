8e3446989efb34b0ebc82d5635f4a40e3140f24e
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-02-05 13:57:01 +0100

    dwl-config/config.def.h

--- dwl-config/config.def.h
+++ dwl-config/config.def.h
@@ -128,7 +128,7 @@ static const Key keys[] = {
 	/* modifier                  key                 function        argument */
 	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
 	{ MODKEY,                    XKB_KEY_Return,     spawn,          {.v = termcmd} },
-	{ MODKEY,                    XKB_KEY_q,          spawn,          {.v = termcmd} },
+	{ MODKEY,                    XKB_KEY_q,          spawn,          {.v = browsercmd} },
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
 	{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
