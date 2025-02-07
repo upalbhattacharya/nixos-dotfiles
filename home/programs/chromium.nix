e369df1c71f91d86325f10b2e30675558e4ba2a9
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-02-07 11:48:16 +0100

    home/programs/chromium.nix

--- home/programs/chromium.nix
+++ home/programs/chromium.nix
@@ -4,6 +4,9 @@
     enable = true;
     extensions = [
       { id = "bkkmolkhemgaeaeggcmfbghljjjoofoh"; } # Catppuccin Mocha
+      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
+      { id = "ekhagklcjbdpajgpjgmbionohlpdbjgc"; } # Zotero Connector
+      { id = "hfjbmagddngcpeloejdejnfgbamkjaeg"; } # Vimium C
     ];
   };
 }
