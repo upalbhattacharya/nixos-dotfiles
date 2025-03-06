fd270cd9072c9ed4afc016af10d463f5cfedb9b3
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-06 16:52:04 +0100

    home/programs/papis.nix

--- home/programs/papis.nix
+++ home/programs/papis.nix
@@ -7,12 +7,14 @@
         name = "full";
         isDefault = true;
         settings = {
-          dir = "~/papis/default/";
+          dir = "~/papis/full/";
         };
       };
     };
     settings = {
       bibtex = {
+        default-read-bibfile = "~/papis/full.bib";
+        default-save-bibfile = "~/papis/full.bib";
       };
     };
   };
