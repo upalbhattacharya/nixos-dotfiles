6d8155c108fff29bc527437f17d8f5d6181ede67
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-15 07:59:37 +0100

    Update files

--- emacs/init.el
+++ emacs/init.el
@@ -73,7 +73,7 @@
  '(org-agenda-todo-keyword-format "%-1s")
  '(org-export-backends '(ascii html icalendar latex odt org))
  '(org-format-latex-options
-   '(:foreground default :background default :scale 1.5 :html-foreground
+   '(:foreground default :background default :scale 1.0 :html-foreground
                  "Black" :html-background "Transparent" :html-scale
                  2.0 :matchers ("begin" "$1" "$" "$$" "\\(" "\\[")))
  '(org-indirect-buffer-display 'current-window)
@@ -83,32 +83,7 @@
      (gnus . org-gnus-no-new-news) (file . find-file)
      (wl . wl-other-frame)))
  '(org-log-into-drawer t)
- '(org-preview-latex-process-alist
-   '((dvipng :programs ("latex" "dvipng") :description "dvi > png"
-             :message
-             "you need to install the programs: latex and dvipng."
-             :image-input-type "dvi" :image-output-type "png"
-             :image-size-adjust (1.0 . 1.0) :latex-compiler
-             ("latex -interaction nonstopmode -output-directory %o %F")
-             :image-converter ("dvipng -D %D -T tight -o %O %F")
-             :transparent-image-converter
-             ("dvipng -D %D -T tight -bg Transparent -o %O %F"))
-     (dvisvgm :programs ("latex" "dvisvgm") :description "dvi > svg"
-              :message
-              "you need to install the programs: latex and dvisvgm."
-              :image-input-type "dvi" :image-output-type "svg"
-              :image-size-adjust (1.7 . 1.5) :latex-compiler
-              ("latex -interaction nonstopmode -output-directory %o %F")
-              :image-converter
-              ("dvisvgm %F --no-fonts --exact-bbox --scale=%S --output=%O"))
-     (imagemagick :programs ("latex" "convert") :description
-                  "pdf > png" :message
-                  "you need to install the programs: latex and imagemagick."
-                  :image-input-type "pdf" :image-output-type "png"
-                  :image-size-adjust (1.0 . 1.0) :latex-compiler
-                  ("pdflatex -interaction nonstopmode -output-directory %o %F")
-                  :image-converter
-                  ("convert -density %D -trim -antialias %F -quality 100 %O"))))
+ '(org-preview-latex-default-process 'dvisvgm)
  '(org-priority-faces
    '((65 :foreground "#181825" :background "#f38ba8")
      (66 :foreground "#181825" :background "#f9e2af")
