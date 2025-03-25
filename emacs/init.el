db244de303928384b61562131568fea1653eb9f3
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-25 13:07:05 +0100

    emacs/init.el

--- emacs/init.el
+++ emacs/init.el
@@ -816,7 +816,7 @@
              '(and
                (todo)
                (not (and
-                     (not (todo "TODAY"))
+                     (not (todo "TODAY" "NOW"))
                      (not (deadline :on today))
                      (not (scheduled :on today))))
                (category "Project" "Area" "Inbox" "Resource" "Literature")
