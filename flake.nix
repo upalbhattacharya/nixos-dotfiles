71ba347705e9abb3a468ee55cc03a3ea019496fd
upalbhattacharya <upal.bhattacharya@gmail.com>
2025-03-26 12:41:54 +0100

    flake.nix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,66 @@
 {
-	description = "First flake";
-	inputs = {
-		nixpkgs = {
-			url = "nixpkgs/nixos-unstable";
-		};
-		catppuccin = {
-			url = "github:catppuccin/nix";
-		};
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
-	outputs = {self, nixpkgs, home-manager, catppuccin, ...}:
-	let
-		lib = nixpkgs.lib;
-		system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages.${system};
-	in {
-		nixosConfigurations = {
-			nixos-workboots = lib.nixosSystem {
-				inherit system;
-				modules = [
-					./configuration.nix
-				];
-			};
-		};
-		homeConfigurations = {
-			workboots = home-manager.lib.homeManagerConfiguration {
-				inherit pkgs;
-				modules = [
-					./home.nix
-	        			catppuccin.homeManagerModules.catppuccin
-				];
-			};
-		};
+  description = "First flake";
+  inputs = {
+    nixpkgs = {
+      url = "nixpkgs/nixos-unstable";
+    };
+    catppuccin = {
+      url = "github:catppuccin/nix";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      catppuccin,
+      zen-browser,
+      ...
+    }@inputs:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = {
+        nixos-workboots = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.workboots = {
+                imports = [
+                  ./home.nix
+                  catppuccin.homeManagerModules.catppuccin
+                ];
+              };
+              home-manager.extraSpecialArgs = {
+                inherit system;
+                inherit inputs;
+              };
+            }
+          ];
+        };
+      };
+      # homeConfigurations = {
+      # 	workboots = home-manager.lib.homeManagerConfiguration {
+      # 		inherit pkgs;
+      # 		modules = [
+      # 			./home.nix
+      #       			catppuccin.homeManagerModules.catppuccin
+      # 		];
+      # 	};
+      # };
 
-	};
+    };
 }
